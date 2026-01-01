025dbf3ad1b7108c8ac2e54f0855756e4e3d5766
XurxoMF <xurxolaxe@gmail.com>
2026-01-01 12:51:41 +0100

    Small changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,24 @@
-<script>
+<script lang="ts">
+	import self from '$lib/assets/slef.webp';
+
 	import { PHCaretDownBold } from '$lib/components/icons';
 </script>
 
 <main>
-	<div
-		class="grid min-h-[calc(100vh-3.5rem)] grid-rows-[1fr_auto] place-items-center gap-8 px-8 text-xl"
-	>
-		<div class="space-y-4 md:max-w-4/5 xl:max-w-2/3">
-			<h1 class="text-5xl font-extrabold xl:text-6xl" id="info">Hi, I'm XurxoMF!</h1>
+	<div class="grid min-h-[calc(100vh-3.5rem)] grid-rows-[1fr_auto] place-items-center gap-8 px-8">
+		<div
+			class="flex w-full flex-col items-center justify-between gap-12 md:w-4/5 lg:flex-row xl:w-2/3"
+		>
+			<h1
+				class=" text-center text-5xl leading-18 font-extrabold lg:text-start xl:text-6xl"
+				id="info"
+			>
+				Hi,
+				<br />
+				I'm <span class="text-pink-500">XurxoMF</span>!
+			</h1>
 
-			<h2 class="text-3xl font-bold text-pink-500 xl:text-5xl">
-				Software Developer & Computer Scientist
-			</h2>
-
-			<p>
-				I'm a 23 years old Galician citizen passionate about programming and technology in general.
-			</p>
-
-			<p>
-				I've been coding since I was 16 and I've gained a lot of experience in this field over the
-				years.
-			</p>
-
-			<p>
-				My main programming languages are <code class="text-pink-500">Node.js</code>,
-				<code class="text-pink-500">Svelte</code>
-				and <code class="text-pink-500">Rust</code> but I also know some other language I'll mention later.
-			</p>
-
-			<p>
-				I've almost 3 years of experience as a Computer Scientists and Web Developer on a company
-				and experience as a Web Development and Software Development in general with a few projects
-				I'll show you later.
-			</p>
+			<img src={self} alt="XurxoMF" class="w-full max-w-80 shrink-0 rounded-full" />
 		</div>
 
 		<div class="flex items-center justify-center p-4">
