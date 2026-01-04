7ea4c359b3a7274679a7debce9429363489c3577
XurxoMF <xurxolaxe@gmail.com>
2026-01-04 22:48:37 +0100

    Added the error page... I forgot it lol

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { A, H1, P } from '$lib/components/typography';
+</script>
+
+<main class="grid min-h-[calc(100vh-7rem)] w-full place-items-center">
+	<div class="flex flex-col items-center justify-center gap-8">
+		<H1>There's nothing here :(</H1>
+		<P>Go back to the <A href="/" color="secondary">home page</A>!</P>
+	</div>
+</main>
